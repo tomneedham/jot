1aa15968b378eb4f013ed79bafee65b401f6c9ec
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-04-17 13:01:48 +0200

    fix app being sorted before Files

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,6 +3,7 @@
 \OCP\App::addNavigationEntry(array(
 
     'id' => 'jot',
+    'order' => 100,
     'href' => \OCP\Util::linkToRoute('jot.page.index'),
     'icon' => \OCP\Util::imagePath('jot', 'icon.svg'),
     'name' => 'Jot',//\OC_L10N::get('Jot')->t('Jot'),
